d042a6a13ef24e6abcbfd744f4cf8eaaaf8aa54b
Starliege <urrianif@gmail.com>
2022-08-23 14:35:24 +0200

    done + bonus

---
+++ main.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+      text: "Hello world",
+      // BONUS: immagine
+      image: "https://wl-brightside.cf.tsp.li/resize/728x/jpg/e54/e78/30a24451a8ba30d37a3dfa888c.jpg"
+    }
+  });
